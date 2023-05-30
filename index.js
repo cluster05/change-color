d60611d739da4d2b13452beeb2b87599ba144355
Prafull Ladha <prafull.ladha@gmail.com>
2023-05-30 21:52:59 +0530

    timeout changes from 5s to 10s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let body = document.getElementsByTagName("body")[0]
 function changeColor(){
     setTimeout(function(){
         body.setAttribute("class","bg")
-    },5000)
+    },10000)
 }
 
 changeColor()
